e8955abfdd5764dd54ea279fefc2fac28067eedf
ramanjaneyalureddyvenna-spec <ramanjaneyalureddyvenna@gmail.com>
2025-11-11 02:27:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// Optionally, you can add effects for menu button, navigation, or WhatsApp button.
+document.querySelector('.menu-btn').addEventListener('click', function() {
+  alert('Menu clicked! (Add navigation overlay here)');
+});
